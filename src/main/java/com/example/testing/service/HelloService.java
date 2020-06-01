4013f1a4372c0dba26f41412da3ee3d5099f1964
wangkang <wangkang@51kik.com>
2020-06-01 16:29:47 +0800

    修复dubbo协议远程调用失败的问题

--- src/main/java/com/example/testing/service/HelloService.java
+++ src/main/java/com/example/testing/service/HelloService.java
@@ -2,6 +2,6 @@ package com.example.testing.service;
 
 public interface HelloService {
 
-    String hello(String name);
+    String hello();
 
 }
--- src/main/java/com/example/testing/service/HelloServiceImpl.java
+++ src/main/java/com/example/testing/service/HelloServiceImpl.java
@@ -1,12 +1,19 @@
 package com.example.testing.service;
 
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @Service
+@RefreshScope
 public class HelloServiceImpl implements HelloService {
-    
+
+    @Value("${local.name}")
+    private String name;
+
     @Override
-    public String hello(String name) {
+    public String hello() {
+        System.out.println(name);
         return "hello " + name;
     }
     
